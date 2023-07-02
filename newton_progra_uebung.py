fc5bc67f8390d3f8023a7cdefe41d91914298eb8
Yagiz Koc <yagizkoc@yahoo.com.tr>
2023-07-02 16:11:25 +0200

    work in progress 4

--- newton_progra_uebung.py
+++ newton_progra_uebung.py
@@ -30,13 +30,16 @@ def f3(x): #in-place
     return np.array([x[0]*0.5*np.sin(np.pi*0.5*x[0])-x[1], pow(x[1], 2)-x[0]+1])
 
 def Df1(x): #in-place ?
-    return np.array([[2*x[0], 2*x[1]], [x[1], x[0]]])
+    return np.array([[2*x[0], 2*x[1]],
+                     [x[1], x[0]]])
 
 def Df2(x): #in-place ?
-    return np.array([[2*x[0], -1],[x[1], x[0]]])
+    return np.array([[2*x[0], -1],
+                     [x[1], x[0]]])
 
 def Df3(x): #in-place ?
-    return np.array([[0.25*x[0]*np.pi*np.cos(np.pi*0.5*x[0])+0.5*np.sin(np.pi*0.5*x[0]), -1], [-1, 2*x[1]]])
+    return np.array([[0.25*x[0]*np.pi*np.cos(np.pi*0.5*x[0])+0.5*np.sin(np.pi*0.5*x[0]), -1],
+                     [-1, 2*x[1]]])
 
 """
 x_ki = []
@@ -60,7 +63,7 @@ x_star = newton(np.array([5]), f4, Df4, 0.0001, 1000, x_ki)
 print(x_star)
 print("")
 for i in x_ki: print(i)
-"""
+
 
 x_startwerte = np.linspace(-2.5, 2.5, num=10000)
 
@@ -74,8 +77,52 @@ fig.suptitle("Grenzwerte für jeden Startwert x0 aus [-2.5, 2.5]")
 axa.plot(x_startwerte, x_star_werte, "_")
 axb.plot(x_startwerte, f4(x_startwerte))
 axb.plot(x_startwerte, np.zeros(x_startwerte.size))
-
+"""
 #c)
 
+def f5(x, j=3):
+    r = np.sqrt(pow(x[0], 2)+pow(x[1], 2))
+    rj = pow(r, j)
+    print(r)
+    phi=0
+    if x[0]>0 and x[1]>0: phi = np.arctan(x[1]/x[0])
+    if x[0]<0 and x[1]>0: phi = np.pi - np.arctan(x[1]/x[0])
+    if x[0]<0 and x[1]<0: phi = np.pi + np.arctan(x[1]/x[0])
+    if x[0]>0 and x[1]<0: phi = np.pi*2 - np.arctan(x[1]/x[0])
+    if x[0]==0:
+        if x[1]>0: phi = np.pi/2
+        if x[1]<0: phi = 3*np.pi/2
+    if x[1]==0:
+        if x[0]>0: phi = 0
+        if x[0]<0: phi = np.pi
+
+    print(phi)
+
+    return np.array([rj*np.cos(j*phi),
+                     rj*np.sin(j*phi)])
+
+def Df5(x, j=3):
+    r = np.sqrt(pow(x[0], 2)+pow(x[1], 2))
+    phi=0
+    if x[0]>0 and x[1]>0: phi = np.arctan(x[1]/x[0])
+    if x[0]<0 and x[1]>0: phi = np.pi - np.arctan(x[1]/x[0])
+    if x[0]<0 and x[1]<0: phi = np.pi + np.arctan(x[1]/x[0])
+    if x[0]>0 and x[1]<0: phi = np.pi*2 - np.arctan(x[1]/x[0])
+    if x[0]==0:
+        if x[1]>0: phi = np.pi/2
+        if x[1]<0: phi = 3*np.pi/2
+    if x[1]==0:
+        if x[0]>0: phi = 0
+        if x[0]<0: phi = np.pi
+
+    return np.array([[j*pow(r, j-1)*np.cos(j*phi),-1*pow(r, j)*j*np.sin(j*phi)],
+                    [j*pow(r, j-1)*np.sin(j*phi), pow(r, j)*j*np.cos(j*phi)]])
+
+
+x_ki = []
+x_star = newton(np.array([0.1,0.1]), f5, Df5, 0.0001, 900, x_ki)
+print(x_star)
+print("")
+for i in x_ki: print(i)
 
 plt.show()
